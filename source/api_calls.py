75bcac4be5f2528c6c4d2d6305a68137da595d2d
Viktoriia-Shalaieva <v.shalaieva@gmail.com>
2024-10-13 19:23:12 +0300

    added structure file

--- source/api_calls.py
+++ source/api_calls.py
@@ -181,7 +181,7 @@ def get_playlist(access_token, playlist_id):
         print(f"The following error occurred: {error_message}")
 
 def get_playlist_items(access_token, playlist_id):
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     headers = {
     "Authorization": f"Bearer {access_token}"
     }
