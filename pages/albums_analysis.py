a67d2023d50a818f6f2d495a8cc3d2185cdbff82
Viktoriia-Shalaieva <v.shalaieva@gmail.com>
2024-12-16 15:42:48 +0200

    Replaced bar plots with functions, added name of artists to the tooltip on the albums analysis page

--- pages/albums_analysis.py
+++ pages/albums_analysis.py
@@ -4,6 +4,7 @@ from modules import components
 import yaml
 import os
 import plotly.express as px
+from modules import plots
 
 
 components.set_page_layout()
@@ -21,6 +22,12 @@ file_paths = {file_name: os.path.join(data_dir, file_name) for file_name in path
 albums_path = str(file_paths['albums.csv'])
 albums_table = pd.read_csv(albums_path, sep='~')
 
+playlists_path = str(file_paths['playlists.csv'])
+playlists_table = pd.read_csv(playlists_path, sep="~")
+
+artists_genres_full_unknown_path = str(file_paths['artists_genres_full_unknown.csv'])
+artists_genres_full_unknown = pd.read_csv(artists_genres_full_unknown_path, sep='~')
+
 albums_table = albums_table.rename(columns={
     'album_id': 'Album ID',
     'album_name': 'Album Name',
@@ -29,6 +36,24 @@ albums_table = albums_table.rename(columns={
     'album_total_tracks': 'Total Tracks',
     'album_label': 'Label',
     'album_popularity': 'Album Popularity'
+})
+
+playlists_table = playlists_table.rename(columns={
+    'playlist_id': 'Playlist ID',
+    'playlist_name': 'Playlist Name',
+    'country': 'Country',
+    'playlist_followers_total': 'Playlist Total Followers',
+    'track_id': 'Track ID',
+    'album_id': 'Album ID',
+    'artist_id': 'Artist ID'
+})
+
+artists_table = artists_genres_full_unknown.rename(columns={
+    'artist_id': 'Artist ID',
+    'artist_name': 'Artist Name',
+    'artist_followers': 'Artist Total Followers',
+    'artist_genres': 'Artist Genres',
+    'artist_popularity': 'Artist Popularity'
 })
 
 fig_histogram = px.histogram(
@@ -45,23 +70,56 @@ st.plotly_chart(fig_histogram)
 
 top_10_albums_popularity = (
     albums_table.nlargest(n=10, columns='Album Popularity')
-    .sort_values(by='Album Popularity', ascending=True)
-)
+    )
 min_x = top_10_albums_popularity['Album Popularity'].min() - 3
-max_x = top_10_albums_popularity['Album Popularity'].max() + 1
+max_x = top_10_albums_popularity['Album Popularity'].max()
 
-fig_top_10_popularity = px.bar(
+# fig_top_10_popularity = px.bar(
+#     top_10_albums_popularity,
+#     x='Album Popularity',
+#     y='Album Name',
+#     orientation='h',
+#     title='Top 10 Most Popular Albums',
+#     # labels={'album_popularity': 'Popularity', 'album_name': 'Album'},
+#     text='Album Popularity',
+#     range_x=[min_x, max_x],
+# )
+# fig_top_10_popularity.update_traces(textposition='outside')
+# st.plotly_chart(fig_top_10_popularity)
+
+merged_playlists_albums = pd.merge(
     top_10_albums_popularity,
+    playlists_table[['Album ID', 'Artist ID']],
+    on='Album ID',
+    how='left'
+)
+merged_playlists_albums.loc[:, 'Artist ID'] = merged_playlists_albums['Artist ID'].str.split(', ')
+expanded_albums_artists = merged_playlists_albums.explode('Artist ID')
+albums_artists_name = expanded_albums_artists.merge(
+    artists_table[['Artist ID', 'Artist Name']],
+    on='Artist ID',
+    how='left'
+)
+st.dataframe(merged_playlists_albums)
+
+top_10_albums_artists_grouped = albums_artists_name.groupby('Album ID').agg({
+    'Album Name': 'first',   # Keep the first occurrence of the track name
+    'Artist Name': lambda x: ', '.join(x.dropna().unique()),  # Concatenate unique artist names, separated by commas
+    'Album Popularity': 'first',
+}).reset_index()
+
+top_10_albums_artists_sorted = top_10_albums_artists_grouped.sort_values(by='Album Popularity', ascending=True)
+
+st.subheader('Top 10 Most Popular Albums')
+plots.create_bar_plot(
+    data=top_10_albums_artists_sorted,
     x='Album Popularity',
     y='Album Name',
     orientation='h',
-    title='Top 10 Most Popular Albums',
-    # labels={'album_popularity': 'Popularity', 'album_name': 'Album'},
     text='Album Popularity',
     range_x=[min_x, max_x],
+    hover_data={'Artist Name': True},
 )
-fig_top_10_popularity.update_traces(textposition='outside')
-st.plotly_chart(fig_top_10_popularity)
 
 # The 'errors="coerce"' argument replaces invalid date entries with NaT (Not a Time),
 # ensuring the column can be processed without raising errors for incorrect formats.
@@ -82,17 +140,25 @@ monthly_releases['Month Name'] = monthly_releases['Month'].map(month_names)
 monthly_releases = monthly_releases.sort_values(by='Month')
 
 
-fig = px.bar(
-    monthly_releases,
+# fig = px.bar(
+#     monthly_releases,
+#     x='Month Name',
+#     y='Release Count',
+#     title='Seasonality of Album Releases',
+#     labels={'Release Count': 'Number of Releases', 'Month Name': 'Month'},
+#     text='Release Count'
+# )
+# fig.update_traces(textposition='outside')
+#
+# st.plotly_chart(fig)
+
+st.subheader('Seasonality of Album Releases')
+plots.create_bar_plot(
+    data=monthly_releases,
     x='Month Name',
     y='Release Count',
-    title='Seasonality of Album Releases',
-    labels={'Release Count': 'Number of Releases', 'Month Name': 'Month'},
-    text='Release Count'
+    text='Release Count',
 )
-fig.update_traces(textposition='outside')
-
-st.plotly_chart(fig)
 
 albums_table['release_year'] = albums_table['Release Date'].dt.year
 
